79eabc7900f4beded5f06af08fbf43d3a1906552
MehKad <mehkadiri@gmail.com>
2023-01-02 19:59:55 +0100

    Frame 101

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        JFrame f1 = new JFrame();
+        f1.setTitle("First Jframe");
+        f1.setSize(420,420);
+        f1.setVisible(true);
+        f1.setResizable(false);
+    }
+}
